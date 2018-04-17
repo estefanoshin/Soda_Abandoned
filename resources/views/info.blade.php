da1867fd7f8d697245c072c5ce9ac01ad8581566
estefanoshin <estefanoshin@yahoo.com.ar>
2018-04-17 15:48:39 -0300

    finnished editing the information

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts.app')
 @section('content')
 
-Info!!!
+<div class="card mb-3">
+  <img id="banner" class="card-img-top" src="soda_banner.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Soda Casual Fashion</h5>
+    <p class="card-text">
+    	SODA es una marca que nace en el 2005, que apuesta cada día por el diseño libre y moderno, la calidad y el confort.
+
+Tiene por objetivo principal acercarse a millones de mujeres que desean verse elegantes y ofrecerles la comodidad de conocernos y adoptarnos desde cualquier lugar.
+
+Todas sus prendas son exclusivas y estudiadas cuidadosamente por medio de un proceso de producción flexible a los cambios que viste a mujeres comunes, modernas y prácticas.
+
+SODA representa la filosofía y la esencia de la feminidad.
+    </p>
+    <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+  </div>
+</div>
 
 @endsection
